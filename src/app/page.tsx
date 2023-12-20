7b1847582e76fc627c4551281a19568276ab0e74
jonas-be <jonasbender2005@gmail.com>
2023-12-20 21:41:33 +0100

    Implement modern hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/project/header";
+import React from "react";
+import Image from 'next/image'
 
 export default async function Home() {
-  return (
-    <main>
-      <Button>TEST</Button>
-    </main>
-  );
+    return (
+        <main>
+            <Header/>
+            <div className={"overflow-hidden"}>
+                <div className="bubble-bg"/>
+                <div className="bubble2"/>
+                <div className="bubble"/>
+            </div>
+
+            <div
+                className={"w-full flex justify-center items-center flex-col-reverse gap-32 md:flex-row md:gap-20 lg:gap-80  xl:gap-[30rem] py-12 md:py-20"}>
+                <div >
+                    <h1 className="text-3xl font-bold text-center flex flex-col items-start gap-2">
+                        <span>Hey I'm</span>
+                        <span
+                            className="text-5xl  drop-shadow-[0_0_0.3rem_#00000030] dark:drop-shadow-[0_0_0.3rem_#ffffff70]">Jonas Bender</span>
+                    </h1>
+                    <p className="mt-4 text-lg">18 y/o Software Developer from Germany</p>
+                </div>
+                <Image
+                    src="/jo-pb.png"
+                    alt="Jonas Logo"
+                    className=" w-32 md:w-auto rounded-full drop-shadow-[0_0_0.3rem_#ffffff70]"
+                    width={190}
+                    height={190}
+                    priority
+                />
+            </div>
+
+            <div className={"w-2 p-2 my-[100vh]"}/>
+
+
+        </main>
+    );
 }
---
+++ src/components/project/header.tsx
@@ -0,0 +1,28 @@
+import {ModeToggle} from "@/components/custom/mode-toggle";
+import React from "react";
+import {buttonVariants} from "@/components/ui/button";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import Image from 'next/image'
+
+
+export default function Header() {
+    return (
+        <>
+            <header
+                className={"bg-background/70 backdrop-blur z-10 flex flex-row justify-between items-center px-4 p-3 fixed w-full "}>
+                <Link href="/" className={cn(buttonVariants({
+                    variant: "ghost", size: "default",
+                    className: "flex flex-row gap-2 items-center px-0 hover:bg-transparent hover:text-primary-foreground/80 group/edit"
+                }))}>
+                    <h1 className="font-semibold text-lg">Jonas Bender</h1>
+                </Link>
+                <div className="flex items-center gap-3">
+                    <ModeToggle/>
+                </div>
+            </header>
+            <div className="w-full h-14 mb-10"/>
+        </>
+
+    )
+}
